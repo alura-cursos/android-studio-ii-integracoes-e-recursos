81a5a7a49dadf8c2ce0a145011792373b8c7956c
caiocmsouza <caiocesar.msouza@gmail.com>
2016-05-30 16:44:02 -0300

    aula2

---
+++ aula2/app/src/main/java/br/com/alura/agenda/modelo/Aluno.java
@@ -0,0 +1,77 @@
+package br.com.alura.agenda.modelo;
+
+import java.io.Serializable;
+
+/**
+ * Created by alura on 12/08/15.
+ */
+public class Aluno implements Serializable {
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String site;
+    private Double nota;
+    private String caminhoFoto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    public String getCaminhoFoto() {
+        return caminhoFoto;
+    }
+
+    public void setCaminhoFoto(String caminhoFoto) {
+        this.caminhoFoto = caminhoFoto;
+    }
+
+    @Override
+    public String toString() {
+        return getId() + " - " + getNome();
+    }
+}
